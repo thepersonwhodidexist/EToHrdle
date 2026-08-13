6c562bb33434d6e5c3522eb6d381e0f7939831d4
thepersonwhodidexist <marlesnonzal@gmail.com>
2026-08-13 13:25:39 -0400

    Update data.js

--- data.js
+++ data.js
@@ -306,6 +306,6 @@ const AREA_IMAGES = {
 
 const AREA_EMBLEMS = {
   "Ring 0 - Purgatorio": "images/emblems/ring0emblem.png",
-  "Ring 1 - Limbo": "images/emblems/ring1emblem.png"
+  "Ring 1 - Limbo": "images/emblems/ring1emblem.png",
   "Forgotten Ridge": "images/emblems/forgottenridgeemblem.png
 };
